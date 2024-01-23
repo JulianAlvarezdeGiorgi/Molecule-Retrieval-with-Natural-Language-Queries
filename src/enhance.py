bd155b3b77b8454d6bc4f0eb48c3b53fab355d80
barthoss <157287606+barthoss@users.noreply.github.com>
2024-01-23 19:36:03 +0100

    Subgraph added in enhance.py

--- src/enhance.py
+++ src/enhance.py
@@ -1,8 +1,13 @@
 """
 Copy each molecule and add 0 at the end of its CID
 Do node dropping augmentation on each molecule and add 1 at the end of its CID
+Do subgraph augmentation on each molecule and add 2 at the end of its CID
 
-TODO : do the same with subgraph (random walk)
+Copy all the other files.
+The train set is enhanced with those two augmentations,
+there are three times more element in the train set
+
+The validation set and test set remains untouched
 """
 
 import os
@@ -12,23 +17,67 @@ import random
 source_data = "/home/admpc/Documents/MVA/S1/altegrad/projet/Public/data/"
 target_data = "/home/admpc/Documents/MVA/S1/altegrad/projet/Public/data_enhanced/"
 
-def node_drop(f_source,f_target,p=0.8) :
+def node_drop(lines,p=0.8) :
     """
-    from a source file compute node drop in target file
-    keep the proportion p of nodes
+    from a CID file lines and a proportion
+    keep p% of the nodes by random selection
+
+    Return a sorted array of the nodes to keep
     """
-    lines=f_source.readlines()
     index=lines.index("idx to identifier:\n")
     nb_nodes=len(lines)-index-1
+
     #construct list of node to keep
     keep=list(range(nb_nodes))
     while len(keep)>int(p*nb_nodes) :
         keep.pop(random.randint(0,len(keep)-1))
+    return keep
+
+def subgraph(lines,p=0.8) :
+    """
+    from a CID file lines and a proportion
+    keep p% of the nodes by random walk
+
+    Return a sorted array of the nodes to keep
+    """
+    index=lines.index("idx to identifier:\n")
+    nb_nodes=len(lines)-index-1
+
+    #construct adjacency list
+    adj={k:set() for k in range(nb_nodes)}
+    for line in lines[1:index-1] :
+        v1,v2=line[:-1].split(" ")
+        v1,v2=int(v1),int(v2)
+        adj[v1].add(v2)
+
+    #construct list of node to keep
+    v=random.randint(0,nb_nodes-1)
+    keep={v}
+    neigh=adj[v].copy()
+    while len(keep)<p*nb_nodes :
+        v=random.choice(list(neigh))
+        if v not in keep :
+            keep.add(v)
+            neigh=neigh.union(adj[v])
+    keep=list(keep)
+    keep.sort()
+    return keep
+
+def write_keep(lines,keep,f_target) :
+    """
+    From a CID file lines, a sorted list of node to keep and a file
+
+    Write the subgraph with reindexing the nodes
+    """
+
     # construct mapping from old idx to new idx
+    index=lines.index("idx to identifier:\n")
+    nb_nodes=len(lines)-index-1
+
     mapping={}
     k=0
     for idx in range(nb_nodes) :
-        if k<int(p*nb_nodes) and idx==keep[k] :
+        if k<len(keep) and idx==keep[k] :
             mapping[str(idx)]=str(k)
             k+=1
         else :
@@ -50,16 +99,12 @@ def node_drop(f_source,f_target,p=0.8) :
         if y is not None:
             f_target.write(y+' '+identifier+'\n')
 
-
-#copy each original molecule and add 0 at the end of the ID
-#do node dropping augmentation and add 1 at the end of the ID
-
 try : os.mkdir(target_data)
 except : pass
 try : os.mkdir(target_data+"raw/")
 except : pass
 
-print("\nCopy single files")
+print("Copy single files")
 shutil.copyfile(source_data+"test_text.txt", target_data+"test_text.txt")
 shutil.copyfile(source_data+"token_embedding_dict.npy", target_data+"token_embedding_dict.npy")
 
@@ -74,6 +119,7 @@ with open(source_data+"train.tsv", 'r') as f_source :
             cid,text=l.split('\t')
             f_target.write(cid+"0\t"+text)
             f_target.write(cid+"1\t"+text)
+            f_target.write(cid+"2\t"+text)
 
 with open(source_data+"val.tsv", 'r') as f_source :
     with open(target_data+"val.tsv", 'w+') as f_target :
@@ -81,15 +127,27 @@ with open(source_data+"val.tsv", 'r') as f_source :
             cid,text=l.split('\t')
             f_target.write(cid+"0\t"+text)
 
-print("\nCopy raw files")
+print("Copy raw files")
 for f in os.listdir(source_data+"raw/") :
     cid=f.split('.')[0]
     shutil.copyfile(source_data+"raw/"+f, target_data+"raw/"+cid+"0.graph")
 
-print("\nNode dropping")
+print("Node dropping")
 for f in os.listdir(source_data+"raw/") :
     if f!= ".DS_Store" :
         cid=f.split('.')[0]
         with open(source_data+"raw/"+f, 'r') as f_source :
-            with open(target_data+"raw/"+cid+"1.graph", 'w+') as f_target :
-                node_drop(f_source,f_target)
+            lines=f_source.readlines()
+        keep=node_drop(lines)
+        with open(target_data+"raw/"+cid+"1.graph", 'w+') as f_target :
+            write_keep(lines,keep,f_target)
+
+print("Sub graph")
+for f in os.listdir(source_data+"raw/") :
+    if f!= ".DS_Store" :
+        cid=f.split('.')[0]
+        with open(source_data+"raw/"+f, 'r') as f_source :
+            lines=f_source.readlines()
+        keep=subgraph(lines)
+        with open(target_data+"raw/"+cid+"2.graph", 'w+') as f_target :
+            write_keep(lines,keep,f_target)
